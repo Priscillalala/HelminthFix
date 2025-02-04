b6b7cea625f6fd84ee3df766e3b88c3bb6c8c20f
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-02-03 16:08:01 -0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Helminth Hatchery Fix
+
+The priority of the Helminth Hatchery post-processing is set too low and it fails to override some important settings. As a result, the stage is much darker in-game than it was in the dev logs. I assume this is a bug. This mod fixes the post-processing priority.
+
+*Thank you to qwp for bringing this to my attention*
+
+## Contact
+You can find me in the [RoR2 Modding Server](https://discord.gg/5MbXZvd) @groove_salad
+
+Or, you can post issues and feedback on the [GitHub](https://github.com/Priscillalala/HatcheryFix/issues)
+
+## Donations
+If you liked my mods, consider buying me a coffee!
+
+<a href="https://www.buymeacoffee.com/groovesalad" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height=60 width=217></a>
---
+++ plugins/HelminthFixPlugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+using System.Security;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+using UnityEngine.Rendering.PostProcessing;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
+
+namespace HelminthFix
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class HelminthFixPlugin : BaseUnityPlugin
+    {
+        public const string
+            GUID = "groovesalad." + NAME,
+            NAME = "HelminthFix",
+            VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        private void SceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+        {
+            if (scene.name == "helminthroost")
+            {
+                GameObject[] rootObjects = scene.GetRootGameObjects();
+                GameObject lighting = Array.Find(rootObjects, x => x.name == "HOLDER: Lighting");
+                if (lighting)
+                {
+                    Transform pp = lighting.transform.Find("Weather, Helminthroost/PP + Amb");
+                    if (pp && pp.TryGetComponent(out PostProcessVolume ppVolume))
+                    {
+                        ppVolume.priority = 5f;
+                    }
+                }
+            }
+        }
+    }
+}
